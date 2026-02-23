7dece6694c98343a0cf00f85e58d7fc83a474504
apolo <apoloprog@gmail.com>
2026-02-23 08:45:08 -0300

    Add files via upload

---
+++ aula1-iw-/variaveis.js
@@ -0,0 +1,20 @@
+var Nome = "apolo"; 
+var Idade = "16";
+var Altura = "1.75";
+var TenhoCabelo = true;
+var Indefinida ;
+
+console.log(Nome);
+console.log(Idade);
+console.log(Altura);
+console.log(TenhoCabelo);
+console.log(Indefinida);
+
+console.log("ola mundo");
+var a = 14;
+var b = 42;
+var tr = (a == 14);
+var fl = (a == b);
+var int1 = tr + 1;
+var int2 = fl + 1;
+
